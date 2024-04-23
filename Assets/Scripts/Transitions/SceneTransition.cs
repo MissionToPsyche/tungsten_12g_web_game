05fe7dc1773ead7206bfd37ba33c6db559ae19b2
prasad2309 <64222390+prasad2309@users.noreply.github.com>
2024-04-23 11:57:20 -0700

    WebGL Build files

--- Assets/Scripts/Transitions/SceneTransition.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class SceneTransition : MonoBehaviour
-{
-    public abstract void InitializeTransition();
-    public abstract void PerformTransition();
-    public abstract void EndTransition();
-}
